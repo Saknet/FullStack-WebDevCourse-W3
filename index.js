a833f01b4f3fc1eca0cf931763ea7794faaf3ce4
Psion <andreas.andra@helsinki.fi>
2018-02-06 18:18:57 +0200

    initate app

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-app.use(cors())
 
 const persons = [
   {
@@ -101,7 +101,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.Port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
