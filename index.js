d36a7ff6ff5ae3750589ccb863efe253e03e2946
Psion <andreas.andra@helsinki.fi>
2018-02-06 18:58:46 +0200

    front + back

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
